3cadb6620dff19d67c30a9ddd8044ceaae08b679
Samet Kahraman <samett1khr@gmail.com>
2022-11-25 17:02:42 +0300

    email gonderilicek kisi ayarlanacak

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -89,3 +89,20 @@ function sendMail() {
       .catch(err=>console.log(err));
   
   }
+// Skill button
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementsByClassName("skillBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+ 
