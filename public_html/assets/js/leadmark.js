046b43f879597d1b5ca6a9958ee1ea8815397e2d
Samet Kahraman <samett1khr@gmail.com>
2022-11-25 11:37:11 +0300

    sixth commit

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -67,3 +67,25 @@ window.onclick = function(event) {
         modal1.style.display = "none";
     }
 }
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_we63pig";
+    const templateID = "template_3px5hwo";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
