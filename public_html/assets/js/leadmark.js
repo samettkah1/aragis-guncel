4ea7609e0906caf58057ac221b914887a51e2e41
Samet Kahraman <samett1khr@gmail.com>
2022-11-30 11:52:59 +0300

    we should set sending email with php

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -41,54 +41,9 @@ $(window).on("load", function() {
 })
 
 
-// Popup Al
-var modal1 = document.getElementById('myModal1');
-
-// Kipi açan düğmeyi al
-var btn = document.getElementById("myBtn1");
-
-// Kipi kapatan <span> öğesini edinin
-var span = document.getElementsByClassName("close1")[0];
 
 
-// Kullanıcı düğmeyi tıklattığında
-btn.onclick = function() {
-    modal1.style.display = "block";
-    
-}
 
-// Kullanıcı <span> (x) düğmesini tıkladığında, popup
-span.onclick = function() {
-    modal1.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-}
-function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_we63pig";
-    const templateID = "template_3px5hwo";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  }
 // Skill button
   function myFunction() {
     var dots = document.getElementById("dots");
